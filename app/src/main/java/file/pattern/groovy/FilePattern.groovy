1ddc62ac8fafcef2bf5e459abb5de2f9f17b76e5
m4gik4rp <72303123+m4gik4rp@users.noreply.github.com>
2023-11-26 18:52:50 +0800

    feat: add file search

--- app/src/main/java/file/pattern/groovy/FilePattern.groovy
+++ app/src/main/java/file/pattern/groovy/FilePattern.groovy
@@ -10,9 +10,19 @@ class Main {
 
     def mainMenu() {
         println("Welcome to text file searcher and replacer")
+        print("Please Enter the file name You want to search: ")
+        def userInput = System.console().readLine()
+        return userInput
+    }
+
+    def backUpFile(){
+
     }
 
     def start() {
+        // Call mainMenu and use as regex pattern
+        def fileName = mainMenu()
+
         // Specify the path to the directory
         def directoryPath = "app/src/main/resources/samplefiles"
 
@@ -26,21 +36,31 @@ class Main {
             println("Directory does not exist.")
         }
 
+
         // Check if the specified path is a directory
         if (directory.isDirectory()) {
             // Get the list of files in the directory
             def fileList = directory.listFiles()
 
-            // Get the list of files in the directory
+           // Get the list of file names in the directory that end with '.txt'
             def txtFiles = directory.listFiles { file ->
                 file.isFile() && file.name.endsWith('.txt')
+            }.collect { file ->
+                file.name
             }
 
-            // Print the names of the .txt files
-            txtFiles.each { txtFile ->
-                println(txtFile.name)
+            println("Matching files: $txtFiles")
+            // Filter and keep only the elements that contain the substring
+            def matchingElements = txtFiles.findAll { element ->
+                element.contains(fileName)
             }
             
+            def counter = 0
+            for (element in matchingElements) {
+                printf("file number %d: %s%n", (counter+1), element, counter++)
+            }
+
+
             } else {
             println("The specified path is not a directory.")
         }
